116b7e8e310f16fe61ee4ce66da7f3e3815cbd95
Jianjun Feng <jianjun.feng@fedex.com>
2021-09-11 21:59:08 -0500

    init

---
+++ src/main/java/com/infosec/samldemo/SamldemoApplication.java
@@ -0,0 +1,13 @@
+package com.infosec.samldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamldemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infosec/samldemo/SamldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosec.samldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
